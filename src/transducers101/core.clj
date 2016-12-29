69d77105c11f158938fbe8c09e48e837a7f83ae3
paul wisehart <paul@oldcode.org>
2016-12-29 11:00:11 -0500

    added simple 'plain-filter-odd' transducer

--- src/transducers101/core.clj
+++
@@ -1,7 +0,0 @@
-(ns transducers101.core
-  (:gen-class))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
---
+++ src/xducer/core.clj
@@ -0,0 +1,33 @@
+(ns xducer.core
+  (:gen-class))
+
+(defn tmpl-xducer [xf]
+	(fn
+		;; set-up
+		([] (xf))
+		;; process
+		([result input] (xf result input))
+		;; tear-down
+		([result] (xf result))))
+
+;; old way:
+;; (->> (range 10)
+;;      (filter odd?)
+;;      (map inc))
+;; transducer way:
+;; (into [] plain-filter-odd (range 10))
+(defn plain-filter-odd [xf]
+	(fn
+		;; set-up
+		([] (xf))
+		;; process
+		([result input] 
+			(cond
+				(odd? input) (xf result (inc input))
+				:else result))
+		;; tear-down
+		([result] (xf result))))
+
+(defn -main [& args]
+  (println "transducers..."))
+
